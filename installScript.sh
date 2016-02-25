ebdb3a1cbde94e49a2191dd2c8ba161bdd0915cf
jorwick <jorwick@gmail.com>
2016-02-25 16:07:43 -0700

    Beta Version

--- installScript.sh
+++ installScript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo UPDATING SYSTEM SOFTWARE
-sudo apt-get update
+sudo apt-get --yes --force-yes update
 echo UPGRADING SYSTEM SOFTWARE
-sudo apt-get upgrade
+sudo apt-get  --yes --force-yes upgrade
 echo UPDATING SOFTWARE DISTRIBUTION
-sudo apt-get dist-upgrade
+sudo apt-get --yes --force-yes dist-upgrade
 echo INSTALL PI-BLASTER
 git clone https://github.com/mwilliams03/pi-blaster.git 
 cd pi-blaster
@@ -13,19 +13,19 @@ sudo make install
 sudo killall pi-blaster
 sudo cp /home/pi/tardisClock/pi-blaster-service /etc/init.d/pi-blaster
 echo INSTALL I2C
-sudo apt-get install i2c-tools
+sudo apt-get --yes --force-yes install i2c-tools
 echo UPDATE Node.js
-sudo apt-get remove nodered
-sudo apt-get remove nodejs nodejs-legacy
+sudo apt-get --yes --force-yes remove nodered
+sudo apt-get --yes --force-yes  remove nodejs nodejs-legacy
 wget http://node-arm.herokuapp.com/node_archive_armhf.deb
 sudo dpkg -i node_archive_armhf.deb
-sudo apt-get install build-essential python-dev python-rpi.gpio
+sudo apt-get --yes --force-yes install build-essential python-dev python-rpi.gpio
 echo INSTALL NPM
-sudo apt-get install npm
+sudo apt-get --yes --force-yes install npm
 echo INSTALL TARDISCLOCK Module dependancies 
 cd /home/pi/tardisClock
 npm install 
 echo Build and Start TardisClock service
-cp /home/pi/tardisClock/TARDISCLOCK.server /etc/systemd/system/TARDISCLOCK.service
+sudo cp /home/pi/tardisClock/TARDISCLOCK.server /etc/systemd/system/TARDISCLOCK.service
 sudo systemctl daemon-reload && sudo systemctl enable TARDISCLOCK.service
 sudo systemctl start TARDISCLOCK
